7f21e823eb608dff70ec9f72b08736557eef9d36
chrisblade33 <barbetchristine@gmail.com>
2022-11-24 15:14:09 +0100

    Ajout signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:new]
   resources :articles
+  get 'signup', to: 'users#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
